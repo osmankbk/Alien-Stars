a6ecdd91285b0a9326521f098a2d2bc51d3af475
osmankbk <mackiekbk@yahoo.com>
2022-07-22 17:25:11 -0400

    Adds functionality to display static file(express.static)

--- server/src/app.js
+++ server/src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const morgan = require('morgan');
 const app = express();
 const planetsRouter = require('./routes/planets/planets.router');
 
 app.use(cors({
   origin: 'http://localhost:3000'
 }));
+app.use(morgan('combined'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.use(planetsRouter);
+app.get('/', (req, res) => {
+  return res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
 
 module.exports = app;
